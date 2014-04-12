580e7ecf1dda4d984f8b4f6f6d95e5fac049d721
Todd Wolfson <todd@twolfson.com>
2014-04-12 15:53:01 -0700

    Completed test skeleton

--- test/fs-memory-store_test.js
+++ test/fs-memory-store_test.js
@@ -1,11 +1,40 @@
-var fs_memory_store = require('../');
+var Store = require('../');
 
-describe('fs-memory-store', function () {
-  before(function () {
+// TODO: Take a base fixture directory
+// TODO: Copy directory to tmp folder
 
+describe('An `fs-memory-store`', function () {
+  describe('loading a non-existent value (from memory and disk)', function () {
+    it('calls back with `null`', function () {
+
+    });
   });
 
-  it('', function () {
+  describe('loading an existent value from disk', function () {
+    it('retrieves the value', function () {
 
+    });
+  });
+
+  describe('saving a value', function () {
+    it('does not have any errors', function () {
+
+    });
+
+    describe('loading the value from memory', function () {
+      it('loads its value', function () {
+
+      });
+    });
+  });
+});
+
+describe('An non-existent value from disk', function () {
+  describe('when overwritten', function () {
+    describe('and loaded again', function () {
+      it('loads its value', function () {
+
+      });
+    });
   });
 });

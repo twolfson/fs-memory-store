2fd19cdc0df29644df3a5abe0387ec88bba617f4
Todd Wolfson <todd@twolfson.com>
2014-04-13 14:15:41 -0700

    Added in mocha-fixture-dir and starting tests

--- test/fs-memory-store_test.js
+++ test/fs-memory-store_test.js
@@ -1,22 +1,27 @@
+// Load in dependencies and library
+var fixtureUtils = require('mocha-fixture-dir')(require('fixture-dir'));
 var Store = require('../');
 
-// TODO: Take a base fixture directory
-// TODO: Copy directory to tmp folder
+// Set up our /tmp namespace
+fixtureUtils.init('fs-memory-store-tests');
 
+// Start our tests
 describe('An `fs-memory-store`', function () {
   describe('loading a non-existent value (from memory and disk)', function () {
+    fixtureUtils.mkdir({folderName: 'non-existent'});
+
     it('calls back with `null`', function () {
 
     });
   });
 
-  describe('loading an existent value from disk', function () {
+  describe.skip('loading an existent value from disk', function () {
     it('retrieves the value', function () {
 
     });
   });
 
-  describe('saving a value', function () {
+  describe.skip('saving a value', function () {
     it('does not have any errors', function () {
 
     });
@@ -29,7 +34,7 @@ describe('An `fs-memory-store`', function () {
   });
 });
 
-describe('An non-existent value from disk', function () {
+describe.skip('An non-existent value from disk', function () {
   describe('when overwritten', function () {
     describe('and loaded again', function () {
       it('loads its value', function () {
